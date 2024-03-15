9517b70e7166af2bb57c06fca8b827ff143f10ec
Oli Dacombe <olidacombe@gmail.com>
2024-03-15 22:13:12 +0000

    start on releases and completion

--- src/cli.rs
+++ src/cli.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use clap_complete::Shell;
 
 #[derive(Parser)]
 #[command(version)]
@@ -10,6 +11,9 @@ pub struct Args {
         help = "compact, i.e. summary only"
     )]
     pub compact: bool,
+    /// Generate a SHELL completion script and print to stdout
+    #[clap(long, value_enum, value_name = "SHELL")]
+    pub completions: Option<Shell>,
     #[arg(short, long, help = "max results to fetch")]
     pub limit: Option<usize>,
     #[arg(
